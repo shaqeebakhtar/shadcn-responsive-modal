0dc720a50ad3d07f7c39c3f15afa5aacb4c75f75
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-11-26 20:19:12 +0530

    add subscription form in modal

--- components/demo-modal.tsx
+++ components/demo-modal.tsx
@@ -12,6 +12,7 @@ import {
   ModalTitle,
   ModalTrigger,
 } from '@/components/ui/modal';
+import { Input } from './ui/input';
 
 function DemoModal() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
@@ -21,23 +22,31 @@ function DemoModal() {
       <ModalTrigger asChild>
         <Button>Open Modal</Button>
       </ModalTrigger>
-      <ModalContent>
+      <ModalContent className="p-4 sm:p-8">
         <ModalHeader>
-          <ModalTitle>Confirm Your Subscription</ModalTitle>
+          <ModalTitle>Subscribe to Our Newsletter</ModalTitle>
           <ModalDescription>
-            You&apos;re about to subscribe to our Premium Plan. Enjoy exclusive
-            features, priority support, and much more for just $9.99/month. Do
-            you want to proceed?
+            Stay updated with the latest news and exclusive offers. Enter your
+            email address below to subscribe to our newsletter.
           </ModalDescription>
         </ModalHeader>
-        <ModalFooter className="flex-col-reverse">
-          <ModalClose asChild>
-            <Button type="button" variant="secondary">
-              Cancel
-            </Button>
-          </ModalClose>
-          <Button>Confirm</Button>
-        </ModalFooter>
+        <form
+          className="space-y-4 mt-1.5"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setIsDialogOpen(false);
+          }}
+        >
+          <Input type="email" id="email" placeholder="shadcn@example.com" />
+          <ModalFooter className="flex-col-reverse p-0">
+            <ModalClose asChild>
+              <Button type="button" variant="secondary">
+                Cancel
+              </Button>
+            </ModalClose>
+            <Button type="submit">Subscribe</Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
