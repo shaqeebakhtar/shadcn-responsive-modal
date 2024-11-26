266add06dc8cb499a3731f3d5ceff713c02b70fb
Shaqeeb Akhtar <shaqeebakhtar01@gmail.com>
2024-11-26 21:22:19 +0530

    change stroke-width to strokeWidth in svg

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
+              strokeWidth="2"
               stroke-linecap="round"
               stroke-linejoin="round"
               className="lucide lucide-github size-4"
